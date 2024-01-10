910b9980bd137e19818b9a9d7e446ea24e949fd5
chriszhao1988 <chriszhao1988@hotmail.com>
2024-01-10 15:03:34 +0800

    add eip712

---
+++ packages/eip712/README.md
@@ -0,0 +1,11 @@
+# EIP712
+
+EIP712 transactions creator.
+
+# Installation
+
+- `npm install @treasurenet/eip712`
+
+
+----
+_TreasureNet 2023_
---
+++ packages/eip712/src/messages/crosschain.ts
@@ -0,0 +1,41 @@
+const MSG_SENDTOETH_TYPES = {
+    MsgValue: [
+        {name: 'sender', type: 'string'},
+        {name: 'eth_dest', type: 'string'},
+        {name: 'amount', type: 'TypeAmount'},
+        {name: 'bridge_fee', type: 'TypeBridgeFee'},
+    ],
+    TypeAmount: [
+        {name: 'denom', type: 'string'},
+        {name: 'amount', type: 'string'},
+    ],
+    TypeBridgeFee: [
+        {name: 'denom', type: 'string'},
+        {name: 'amount', type: 'string'},
+    ]
+};
+
+export function createMsgSendToEth(
+    sender: string,
+    eth_dest: string,
+    amount: string,
+    denom: string,
+    bfAmount: string,
+    bfDenom: string
+) {
+    return {
+        type: 'gravity/MsgSendToEth',
+        value: {
+            amount: {
+                amount,
+                denom,
+            },
+            bridge_fee: {
+                amount: bfAmount,
+                denom: bfDenom,
+            },
+            sender,
+            eth_dest,
+        },
+    };
+}
